1e92d3c86ef253cf5f6cb35c0d8817b6eacd598e
alfredo del canto <a.delcanto@outlook.com>
2016-07-11 11:16:33 +0200

    Project Random Quote Generator Basic Version

---
+++ js/quotes.js
@@ -0,0 +1,28 @@
+var quotes = [
+{
+  quote: 'You can do anything but not everything',
+  source: 'David Allen',
+  citation: 'Making It All Work',
+  year: 2009
+},
+{
+  quote: 'Sometimes you will never know the value of a moment until it becomes a memory',
+  source: 'Dr. Suess',
+},
+{
+  quote: 'Life is to be lived, not controlled; and humanity is won by continuing to play in face of certain defeat.',
+  source: 'Ralph Ellison',
+  citation: 'Invisible Man',
+  year: 1952
+},
+{
+  quote: 'It sounds plausible enough tonight, but wait until tomorrow. Wait for the common sense of the morning.',
+  source: 'H.G.Wells',
+  citation: 'The Time Machine',
+  year: 1895
+},
+{
+  quote: 'The true sign of intelligence is not knowledge but imagination',
+  source: 'Albert Eisntein'
+}
+];
---
+++ js/script.js
@@ -0,0 +1,26 @@
+// function to select and return a random quote object from the quotes array
+function getRandomQuote (num) {
+  var randomNum = Math.floor(Math.random() * num);
+  return quotes[randomNum];
+}
+
+// function to call geRandomQuote and construct a string with the different properties
+function printQuote () {
+  var randQuote = getRandomQuote(quotes.length);
+  var message = '<p class="quote">' + randQuote.quote + '</p>';
+  message += '<p class="source">' + randQuote.source ;
+  if (randQuote.citation != null) {
+    message += '<span class="citation">' + randQuote.citation + '</span>';
+  }
+  if (randQuote.year != null) {
+    message += '<span class="year">' + randQuote.year + '</span>';
+  }
+  message += '</p>';
+  document.getElementById('quote-box').innerHTML = message;
+}
+
+// call to function printQuote for random quote appearing when opening site
+printQuote();
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
