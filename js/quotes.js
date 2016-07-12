5f1900926de981a5b06cc823f60aa2ec53128b1f
alfredo del canto <a.delcanto@outlook.com>
2016-07-12 12:04:31 +0200

    Add functionality

--- js/quotes.js
+++ js/quotes.js
@@ -1,28 +1,30 @@
 var quotes = [
 {
-  quote: 'You can do anything but not everything',
+  quote: '1. You can do anything but not everything',
   source: 'David Allen',
   citation: 'Making It All Work',
-  year: 2009
+  year: 2009,
+  tags: 'Motivation'
 },
 {
-  quote: 'Sometimes you will never know the value of a moment until it becomes a memory',
+  quote: '2. Sometimes you will never know the value of a moment until it becomes a memory',
   source: 'Dr. Suess',
 },
 {
-  quote: 'Life is to be lived, not controlled; and humanity is won by continuing to play in face of certain defeat.',
+  quote: '3. Life is to be lived, not controlled; and humanity is won by continuing to play in face of certain defeat.',
   source: 'Ralph Ellison',
   citation: 'Invisible Man',
   year: 1952
 },
 {
-  quote: 'It sounds plausible enough tonight, but wait until tomorrow. Wait for the common sense of the morning.',
+  quote: '4. It sounds plausible enough tonight, but wait until tomorrow. Wait for the common sense of the morning.',
   source: 'H.G.Wells',
   citation: 'The Time Machine',
   year: 1895
 },
 {
-  quote: 'The true sign of intelligence is not knowledge but imagination',
-  source: 'Albert Eisntein'
+  quote: '5. The true sign of intelligence is not knowledge but imagination',
+  source: 'Albert Eisntein',
+  tags: 'Intelligence'
 }
 ];
--- js/script.js
+++ js/script.js
@@ -1,26 +1,57 @@
+var arr = quotes.slice(0);
+var randomNum;
+
 // function to select and return a random quote object from the quotes array
 function getRandomQuote (num) {
-  var randomNum = Math.floor(Math.random() * num);
-  return quotes[randomNum];
+      randomNum = Math.floor(Math.random() * num);
+      return arr[randomNum];
+    }
+
+// function to generate random number between 0 and 256
+function getRandomNumber () {
+  return Math.floor(Math.random() * 256);
+}
+
+// function to generate random color and change background and button color
+function getRandomColor () {
+  var randColor = 'rgb(' + getRandomNumber() + ', ' + getRandomNumber() + ', ' + getRandomNumber() + ')'
+  document.getElementById('bodyStyle').style.backgroundColor = randColor;
+  document.getElementById('loadQuote').style.backgroundColor = randColor;
+}
+
+// functio to remove sected quote and reload array with quotes when empty
+function modifyArr () {
+  arr.splice(arr.indexOf(arr[randomNum]),1);
+  if (arr.length < 1) {
+    arr = quotes.slice(0);
+  }
 }
 
 // function to call geRandomQuote and construct a string with the different properties
 function printQuote () {
-  var randQuote = getRandomQuote(quotes.length);
-  var message = '<p class="quote">' + randQuote.quote + '</p>';
-  message += '<p class="source">' + randQuote.source ;
-  if (randQuote.citation != null) {
-    message += '<span class="citation">' + randQuote.citation + '</span>';
-  }
-  if (randQuote.year != null) {
-    message += '<span class="year">' + randQuote.year + '</span>';
-  }
-  message += '</p>';
-  document.getElementById('quote-box').innerHTML = message;
+    console.log(arr);
+    var randQuote = getRandomQuote(arr.length);
+    modifyArr();
+    var message = '<p class="quote">' + randQuote.quote + '</p>';
+    message += '<p class="source">' + randQuote.source ;
+    if (randQuote.citation != null) {
+      message += '<span class="citation">' + randQuote.citation + '</span>';
+    }
+    if (randQuote.year != null) {
+      message += '<span class="year">' + randQuote.year + '</span>';
+    }
+    if (randQuote.tags != null) {
+      message += '<span class="citation">' + randQuote.tags + '</span>';
+    }
+    message += '</p>';
+    getRandomColor();
+    document.getElementById('quote-box').innerHTML = message;
+
 }
 
 // call to function printQuote for random quote appearing when opening site
 printQuote();
+
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
